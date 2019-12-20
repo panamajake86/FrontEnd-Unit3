68b25e58ba27be6774c8f8c3f3ac05bec8ab6b28
Jake <panamajake86@gmail.com>
2019-12-19 20:15:35 -0700

    getting routes and paths set up

--- stories/src/App.js
+++ stories/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path='/portal' component={Admin} />
+        <Route path='/aboutus' component={AboutUs} />
+        <Route path='/stories' component={Stories} />
+        <Route path='/submit' component={Submit} />
+    </Router>
   );
 }
 
