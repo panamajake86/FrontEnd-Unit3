5f4ff590d7f8255057366a3b253763822f7d7487
Chris Daley <Daleys@Chriss-iMac.local>
2019-12-20 21:00:12 -0700

    created a component StoriesCarousel. This componenet will cylcle thru images. I created a code that will cycle thru the images every 3.5 seconds or you can manually click thru the carousel. I uploaded some test images thru unsplash and created a file in SRC called imagesTEst where the images are being stored.

--- stories/src/App.js
+++ stories/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import Carousel from "./components/StoriesCarousel";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/carousel-stories" component={Carousel} />
       </div>
     </Router>
   );
