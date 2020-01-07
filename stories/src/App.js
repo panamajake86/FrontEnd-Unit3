80344ee89acce16cecd802af82e21f2d8b988e5c
Batuhan Balta <bthn.balta@gmail.com>
2020-01-06 21:56:40 -0800

    routes-updated

--- stories/src/App.js
+++ stories/src/App.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import Stories from "./components/Stories";
+import SubmitStories from "./components/SubmitStories";
+import Navigation from "./components/Navigation";
 import Carousel from "./components/StoriesCarousel";
-
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <h1 className="header">Refugee Stories</h1>
+          <div className="nav-links">
+            <Navigation />
+          </div>
+        </nav>
         <Route exact path="/" component={Login} />
+        <Route path="/stories" component={Stories} />
+        <Route path="/submit" component={SubmitStories} />
+
         <PrivateRoute exact path="/carousel-stories" component={Carousel} />
       </div>
     </Router>
--- stories/src/components/Navigation.js
+++ stories/src/components/Navigation.js
@@ -1,3 +1,9 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Navigation = () => {
+  return <div>Navigation</div>;
+};
+
+export default Navigation;
