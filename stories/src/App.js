5f22bc92c950177e4cd4f7de5ecf8bf61195b84f
Jake <panamajake86@gmail.com>
2020-01-06 19:58:28 -0700

    got some of those contexts set up.

--- stories/src/App.js
+++ stories/src/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import { axiosWithAuth } from './axiosWithAuth';
+import { PubContext } from './contexts/PubStories';
 
 function App() {
+  const { pub, setPub } = useState({});
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('')
+      .then(res => setPub(res.data))
+      .catch(err => console.log(err.message));
+  });
+
   return (
-    <Router>
+    <PubContext.Provider value={pub}>
+      <Router>
         <Route path="/login" component={Login} />
         <PrivateRoute exact path='/portal' component={Admin} />
         <Route path='/aboutus' component={AboutUs} />
         <Route path='/stories' component={Stories} />
         <Route path='/submit' component={Submit} />
-    </Router>
+      </Router>
+    </PubContext.Provider>
   );
 }
 
